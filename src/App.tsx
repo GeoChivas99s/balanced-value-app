9855d70b5462616efd328ba423920d58a236ce93
geoChivas99 <chivasc95@gmail.com>
2023-06-28 16:42:58 +0100

    feat: add balanced option

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { isBalancedExpression } from "./utils";
 function App() {
+  const [value, setValue] = useState<string>();
+
+  console.log(value);
+  useEffect(() => {}, [value]);
+
   return (
     <div className="App">
-     <input  className='main-input' placeholder='Insira a sua expressão ... ex. '/>
+      <input
+        className="main-input"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Insira a sua expressão ..."
+      />
+
+      {value && (
+        <p>
+          A expressão {isBalancedExpression(value) ? "é" : "não é "} balanceada{" "}
+        </p>
+      )}
     </div>
   );
 }
--- src/utils/isBalancedExpression.ts
+++ src/utils/isBalancedExpression.ts
@@ -1,23 +1,26 @@
 function isBalancedExpression(expression: string): boolean {
-    const stack: string[] = [];
-    const parentheses: { [key: string]: string } = {
-        '(': ')',
-        '[': ']',
-        '{': '}'
-    };
+  const stack: string[] = [];
+  const parentheses: { [key: string]: string } = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
 
-    for (let char of expression) {
-        if (parentheses[char]) {
-            stack.push(char);
-        } else if (char === ')' || char === ']' || char === '}') {
-            const lastOpenedParentheses = stack.pop();
-            if (!lastOpenedParentheses || parentheses[lastOpenedParentheses] !== char) {
-                return false;
-            }
-        }
+  for (let char of expression) {
+    if (parentheses[char]) {
+      stack.push(char);
+    } else if (char === ")" || char === "]" || char === "}") {
+      const lastOpenedParentheses = stack.pop();
+      if (
+        !lastOpenedParentheses ||
+        parentheses[lastOpenedParentheses] !== char
+      ) {
+        return false;
+      }
     }
+  }
 
-    return stack.length === 0;
+  return stack.length === 0;
 }
 
 export default isBalancedExpression;
